baa415b060d8bd9393185fff2a01f0b51dd323fd
iamharisgul <harisgul2999@gmail.com>
2023-04-06 09:05:26 +0500

    initial project base

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #custom apps
+    'authentication',
+    'finance',
 ]
 
 MIDDLEWARE = [
